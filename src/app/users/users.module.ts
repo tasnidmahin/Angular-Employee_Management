eff2ee507862e1b5732a17fdbcea5f9ea76fcf3b
Tasnid Mahin <tasnidmahin@gmail.com>
2023-12-30 11:18:32 +0600

    EM-101: Fix delete and delete icon issue
    - Show edit and delete icon properly
    - Import PrimeNg Button Module

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersComponent } from './users.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [UsersComponent],
-  imports: [UsersRoutingModule, CommonModule, TableModule]
+  imports: [UsersRoutingModule, CommonModule, TableModule, ButtonModule]
 })
 export class UsersModule {}
