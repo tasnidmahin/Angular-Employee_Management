49708c50172e891c58365ebc949060b8df062965
Tasnid Mahin <tasnidmahin@gmail.com>
2024-02-04 21:58:44 +0600

    EM-104: Show user Id in UI

--- src/app/users/users-profile/users-profile.component.ts
+++ src/app/users/users-profile/users-profile.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'aem-users-profile',
   templateUrl: './users-profile.component.html',
   styleUrls: ['./users-profile.component.scss']
 })
-export class UsersProfileComponent {
+export class UsersProfileComponent implements OnInit{
+  userId: number | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.userId = params['id'];
+    });
+  }
 
 }
